32dc1d8ac63d46e1e297d249ce3d846bc10cab78
mohamadmaoya248-sudo <mohamadmaoya248@gmail.com>
2026-02-12 14:54:36 +0200

    Create app.py
    Add app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "مرحباً بك في نظام التداول بالذكاء الاصطناعي!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
